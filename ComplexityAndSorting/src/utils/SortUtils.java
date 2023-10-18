ae8162a5355a059f42bb39e4528de2eb372ca1a8
Savanna Piscitelli <119908631+SavannaPisc@users.noreply.github.com>
2023-10-18 14:05:49 -0600

    Add insertion sort and heap sort into SortUtils.java

--- ComplexityAndSorting/src/utils/SortUtils.java
+++ ComplexityAndSorting/src/utils/SortUtils.java
@@ -59,7 +59,74 @@ public class SortUtils {
 
         return i;
     }
+    
+    // Insertion Sort
+	public static void insertionSort(Shape[] shapes, String sortType)
+	{
+		for (int i = 1; i < shapes.length; i++) 
+		{
+			int key = shapes[i];
+			int j = i - 1;
+			
+			while (j >= 0 && shapes[j] < key) 
+			{
+				// Swap shapes[j + 1] and shapes[j]
+				shapes[j + 1] = shapes[j];
+				j = j - 1;
+			}
+			shapes[j + 1] = key;
+		}
+	}
+	
+	// Heap Sort
+	public static void heapSort(Shape[] shapes, String sortType) 
+	{
+		for (int i = shapes.length - 2; i >= 0; i--)
+		{
+			heapify(shapes, shapes.length, i);
+		}
+		
+		for (int i = shapes.length - 1; i >= 0; i--) 
+		{
+			// Swap shapes at index 0 and i
+			int temp = shapes[0];
+			shapes[0] = shapes[i];
+			shapes[i] = temp;
+			
+			heapify(shapes, i, 0);
+		}
+	}
+	
+	public static void heapify(Shape[] shapes, String sortType, int l, int i) 
+	{
+		int min = i;
+		int left = i + 1;
+		int right = i + 2;
+		
+		if (left < l && shapes[left] < shapes[min]) 
+		{
+			min = left;
+		}
 
+		else 
+		{
+			min = i;
+		}
+		
+		if (right < l && shapes[right] < shapes[min])
+		{
+			min = right;
+		}
+		
+		if (min != i) {
+			int temp = shapes[i];
+			shapes[i] = shapes[min];
+			shapes[min] = temp;
+			
+			heapify(array, l, min);
+		}
+	}
+    
     private static boolean shouldSwap(Shape a, Shape b, String sortType) {
         switch (sortType) {
             case "v":
